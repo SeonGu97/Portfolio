ce4a0aa291a4397cab3342a759c9ff5a432f4f26
강선구 <seongu97@naver.com>
2022-07-21 18:54:25 +0900

    Docs(dark_mod): 엘리먼트 추가

---
+++ js/header/dark_mod.js
@@ -0,0 +1,34 @@
+'use strict';
+
+export default class Dark_mod {
+    constructor(Creator, header) {
+        this.dark_mod = new Creator('dark_mod', 'label', ['class'], ['dark_mod bs'], '', 1, header, false, '', '');
+    
+        this.mod_icon = new Creator('mod_icon', 'span', ['class'], ['mod_icon'], '', 1, this.dark_mod.name, true, 'click', this.modEvent);
+        
+        this.icon = ['🌝'];
+
+        this.mod_icon.name.innerHTML = this.icon[0];
+    }
+
+    modEvent(e) {
+        this.target = e.target;
+
+        this.target.parentElement.classList.toggle('mod_bg');
+        this.target.classList.toggle('mod_change');
+
+        document.body.classList.toggle('dark');
+
+        this.icon = ['🌝', '🌚'];
+
+        this.analog = document.querySelector('.analog');
+
+        if(this.target.classList.contains('mod_change')) {
+            this.target.innerHTML = this.icon[1];
+            this.analog.style.borderColor = '#E2F3F6';
+        }else {
+            this.target.innerHTML = this.icon[0];
+            this.analog.style.borderColor = '#333536';
+        }
+    }
+}
--- js/header/header.js
+++ js/header/header.js
@@ -1,7 +1,11 @@
 'use strict';
 
+import Dark_mod from "./dark_mod.js";
+
 export default class Header {
     constructor(Creator, main) {
         this.header = new Creator('header', 'header', [''], [''], '', 1, main, false, '', '');
+    
+        this.dark_mod = new Dark_mod(Creator, this.header.name);
     }
 }
